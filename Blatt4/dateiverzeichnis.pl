51b8d0b4e06d5762a76b7d7bbc416d242fa7628b
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2020-01-20 15:57:08 +0100

    Blatt4 Ski-Start

---
+++ Blatt4/dateiverzeichnis.pl
@@ -0,0 +1,154 @@
+
+:- dynamic(directory/5).
+:- dynamic(file/6).
+
+% directory(DirId,Name,ParentId,DateCreated,DateModified)
+
+directory(1,root,0,date(2007,5,2),date(2007,5,2)).
+directory(2,bilder,1,date(2007,5,2),date(2009,11,2)).
+directory(3,musik,1,date(2007,5,2),date(2009,10,4)).
+directory(4,dokumente,1,date(2007,5,2),date(2009,11,5)).
+directory(5,urlaub,2,date(2008,6,22),date(2009,8,15)).
+directory(6,hochzeit,2,date(2008,1,27),date(2008,1,31)).
+directory(7,kinder,2,date(2007,5,2),date(2009,9,5)).
+directory(8,klassik,3,date(2007,5,2),date(2007,5,2)).
+directory(9,pop,3,date(2007,5,2),date(2009,11,5)).
+directory(10,urlaub,4,date(2008,5,23),date(2008,11,1)).
+directory(11,hochzeit,4,date(2007,12,4),date(2008,1,25)).
+directory(12,scheidung,4,date(2009,9,2),date(2009,11,5)).
+
+% Änderungsdatum-Aktualisierung
+directory(1337,springseil,8,date(2009,9,2),date(2009,11,5)).
+
+% file(FileId,DirId,Name,Size,DateCreated,DateModified)
+
+file(1,9,in_the_summertime,56,date(2007,5,2),date(2009,11,5)).
+file(2,9,i_am_so_romantic_tonight,31,date(2007,5,2),date(2009,11,5)).
+file(3,9,ich_und_du_fuer_immer,67,date(2007,5,2),date(2009,11,5)).
+file(4,9,paris,267,date(2008,6,3),date(2008,6,3)).
+file(7,10,quartieranfrage,1,date(2007,5,2),date(2009,11,5)).
+file(13,5,paris,251,date(2008,6,22),date(2008,6,17)).
+file(14,5,dijon,217,date(2008,6,22),date(2008,6,17)).
+file(15,5,die_bruecke_von_avignon,191,date(2008,6,22),date(2008,6,17)).
+file(21,6,polterabend,238,date(2008,1,27),date(2008,1,31)).
+file(22,6,standesamt,244,date(2008,1,27),date(2008,1,31)).
+file(23,6,kirche,158,date(2008,1,28),date(2008,1,31)).
+file(24,6,festessen,151,date(2008,1,28),date(2008,1,31)).
+file(25,11,standesamt,33,date(2007,6,3),date(2007,6,3)).
+file(34,12,scheidungsklage,48,date(2009,9,2),date(2009,11,5)).
+
+
+
+% directory(DirId, Name, ParentId, DateCreated, DateModified)
+% file(FileId, DirId, Name, Size, DateCreated, DateModified)
+
+% Dateinamen zu Dateischlüssel
+
+dateinameZuID(Dateiname,FileID) :- 
+
+file(FileID,_,Dateiname,_,_,_). 
+
+
+% Verzeichnisname zu Verzeichnisschlüssel
+
+verzeichnisnameZuID(Verzeichnisname,DirID) :- 
+
+directory(DirID,Verzeichnisname,_,_,_).
+
+
+% Dateiname zu Name und Schlüssel des Verzeichnisses
+
+dateinameZuDirNameUndID(Dateiname,DirName,DirID) :- 
+
+file(_,DirID,Dateiname,_,_,_),
+directory(DirID,DirName,_,_,_).
+
+
+% Verzeichnisname zu Verzeichnisname und Schlüssel des obigen Verzeichnisses
+
+verzeichnisnameZuNameIDAbove(DirName,DirNameAbove,ParentID) :- 
+
+directory(_,DirName,ParentID,_,_),
+directory(ParentID,DirNameAbove,_,_,_).
+
+% Liste aller Dateinamen eines Verzeichnisses 
+% Identifikation per ID
+
+listeAllerDateinamen(DirID,DateinamenListe) :-
+
+findall(FileNames, 
+		file(_,DirID,FileNames,_,_,_),
+		DateinamenListe).
+
+
+% Liste aller Namen der Unterverzeichnisse eines Verzeichnisses 
+% Identifikation per ID
+
+listeAllerUnterverzeichnisse(DirName,Unterverzeichnisliste) :-
+
+findall(DirNameName, 
+
+		(directory(_,DirNameName,ID,_,_),
+		verzeichnisnameZuID(DirName,ID)),
+
+		Unterverzeichnisliste).
+
+
+% Anzahl aller Dateien in einem bestimmten Verzeichnis
+% Identifikation per ID
+
+anzahlAllerDateien(DirID, Anzahl) :-
+
+listeAllerDateinamen(DirID,Liste),
+length(Liste,Anzahl).
+
+
+% Änderungsdatum eines Verzeichnisses auf das aktuelle Datum setzen
+% Fehlerbehandlung inklusive
+
+% TODO Fehlerbehandlung 
+
+setzeAenderungsdatumAktuell(DirID) :- 
+
+
+retract(directory(DirID,Name,ParentID,DateCreated,_)),
+assert(directory(DirID,Name,ParentID,DateCreated,date(29,10,1991))).
+
+%catch(setzeAenderungsdatumAktuell(DirID,Fehlercode)), 
+%Fehlercode,
+%format('Verzeichnis exsitiert nicht!',Fehlercode),
+%retract(directory(DirID,Name,ParentID,DateCreated,_)),
+%assert(directory(DirID,Name,ParentID,DateCreated,date(29,10,1991))).
+
+
+% Hinzufügen eines Unterverzeichnisses
+
+% TODO 
+% Doppeleintragungen verhindern 
+% Modifikationsdaten aktualisieren
+
+unterverzeichnisHinzufuegen(DirID,Name) :- 
+
+dirIDerhoehen(ID),
+assert(directory(ID,Name,DirID,_,_)).
+
+
+% IDs hochzählen
+
+dirIDerhoehen(ID) :- 
+
+flag(directories,ID,ID+1).
+
+
+
+% Existiert ein Zugriffspfad
+
+% TODO
+
+%zugriffspfadExistent(DirID1, DirID2, Result) :-
+
+
+
+
+
+
